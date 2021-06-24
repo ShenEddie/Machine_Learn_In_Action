686e883d157d61f31d134f3948fcaa8a2da10e8e
ShenEddie <sheneddie@outlook.com>
2021-06-24 16:54:32 +0800

    Decision trees run split data

--- CH03/run.py
+++ CH03/run.py
@@ -1,8 +1,11 @@
 import trees
 
-my_data, labels = trees.create_dataset()
-
+my_data, _ = trees.create_dataset()
 print(trees.cal_shannon_ent(my_data))
 
 my_data[0][-1] = 'maybe'
 print(trees.cal_shannon_ent(my_data))
+
+my_data, labels = trees.create_dataset()
+trees.split_dataset(my_data, 0, 1)
+trees.split_dataset(my_data, 0, 0)
