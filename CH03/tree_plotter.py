9333ff7fa7cedab804ee225a25f2c979c5de8968
ShenEddie <sheneddie@outlook.com>
2021-06-25 18:29:49 +0800

    Decision trees plot_node & create_plot

---
+++ CH03/tree_plotter.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from typing import Tuple, Dict
+
+decision_node = dict(boxstyle="sawtooth", fc="0.8")
+leaf_node = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+
+def plot_node(node_text: str,
+              center_point: Tuple[float, float],
+              parent_point: Tuple[float, float],
+              node_type: Dict):
+    create_plot.ax1.annotate(node_text, xy=parent_point,
+                             xycoords='axes fraction',
+                             xytext=center_point, textcoords='axes fraction',
+                             va="center", ha="center", bbox=node_type,
+                             arrowprops=arrow_args)
+
+
+def create_plot():
+    fig: plt.Figure = plt.figure(1, facecolor='white')
+    fig.clf()
+    create_plot.ax1 = plt.subplot(111, frameon=False)
+    plot_node('a decision node', (0.5, 0.1), (0.1, 0.5), decision_node)
+    plot_node('a leaf node', (0.8, 0.1), (0.3, 0.8), leaf_node)
+    plt.show()
